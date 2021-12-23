f3badeeaf5a8178980b632dc2dd31cb8eda34b75
Developer-Nasim <ajnasim72@gmail.coom>
2021-12-24 04:53:25 +0600

    responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,122 +6,52 @@
     $('.mobile-menu').toggleClass('siteBar');   
   }); 
 
-  
-  // mainSlider
-  function mainSlider() {
-    var BasicSlider = $(".slider-active");
-    BasicSlider.on("init", function(e, slick) {
-      var $firstAnimatingElements = $(".single-slider:first-child").find(
-        "[data-animation]"
-      );
-      doAnimations($firstAnimatingElements);
-    });
-    BasicSlider.on("beforeChange", function(e, slick, currentSlide, nextSlide) {
-      var $animatingElements = $(
-        '.single-slider[data-slick-index="' + nextSlide + '"]'
-      ).find("[data-animation]");
-      doAnimations($animatingElements);
-    });
-    BasicSlider.slick({
-      autoplay: false,
-      autoplaySpeed: 10000,
-      dots: false,
-      fade: true,
-      arrows: true,
-      prevArrow:
-        '<button type="button" class="slick-prev"><i class="ti-shift-left"></i></button>',
-      nextArrow:
-        '<button type="button" class="slick-next"><i class="ti-shift-right"></i></button>',
-      responsive: [
-        { breakpoint: 767, settings: { dots: false, arrows: false } }
-      ]
-    });
+  // Timmer
+  function Timmer() {
+    
+     // Set the date we're counting down to
+     var countDownDate = new Date("Dec 31, 2021 15:37:25").getTime();
 
-    function doAnimations(elements) {
-      var animationEndEvents =
-        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-      elements.each(function() {
-        var $this = $(this);
-        var $animationDelay = $this.data("delay");
-        var $animationType = "animated " + $this.data("animation");
-        $this.css({
-          "animation-delay": $animationDelay,
-          "-webkit-animation-delay": $animationDelay
-        });
-        $this.addClass($animationType).one(animationEndEvents, function() {
-          $this.removeClass($animationType);
-        });
-      });
-    }
+     // Update the count down every 1 second
+     var x = setInterval(function() {
+
+     // Get todays date and time
+     var now = new Date().getTime();
+
+     // Find the distance between now and the count down date
+     var distance = countDownDate - now;
+
+     // Time calculations for days, hours, minutes and seconds
+     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+     
+     document.querySelector(".days").innerHTML = days;
+     document.querySelector(".hours").innerHTML = hours;
+     document.querySelector(".minutes").innerHTML = minutes;
+     document.querySelector(".seconds").innerHTML = seconds;
+
+     // If the count down is finished, write some text 
+     if (distance < 0) {
+         clearInterval(x);
+         document.getElementById("timmers").innerHTML = "EXPIRED";
+     }
+     }, 1000); 
   }
-  mainSlider();
-
-
-  // owlCarousel
-  $(".brand-active").owlCarousel({
-    loop: true,
-    margin: 30,
-    items: 6,
-    navText: [
-      '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ],
-    nav: false,
-    dots: false,
-    responsive: {
-      0: {
-        items: 2
-      },
-      767: {
-        items: 3
-      },
-      992: {
-        items: 6
-      }
-    }
-  });
-
-  /* magnificPopup img view */
-  $(".popup-image").magnificPopup({
-    type: "image",
-    gallery: {
-      enabled: true
-    }
-  }); 
-
-  // counter
-  $('.counter').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
-  // isotop
-  $(".grid").imagesLoaded(function() {
-    // init Isotope
-    var $grid = $(".grid").isotope({
-      itemSelector: ".grid-item",
-      percentPosition: true,
-      masonry: {
-        // use outer width of grid-sizer for columnWidth
-        columnWidth: ".grid-item"
-      }
-    });
-
-    // filter items on button click
-    $(".portfolio-menu").on("click", "button", function() {
-      var filterValue = $(this).attr("data-filter");
-      $grid.isotope({ filter: filterValue });
-    });
-
-    //for menu active class
-    $(".portfolio-menu button").on("click", function(event) {
-      $(this)
-        .siblings(".active")
-        .removeClass("active");
-      $(this).addClass("active");
-      event.preventDefault();
-    });
-  });
-  
+  Timmer()
+    
+  // ScrollTop
+  function ScrollToTop() {
+    let btn = document.querySelector('.scrollToTop')
+    window.addEventListener('scroll', () => { 
+        if (window.scrollY > 500) {
+            btn.classList.add('show')
+        }else{ 
+            btn.classList.remove('show')
+        }
+    })
+  }
+  ScrollToTop()
  
 })(jQuery);
